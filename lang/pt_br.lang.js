c531ec931d543892f55de369cae6499d1699ba22
knoonrx <k-noon@hotmail.com>
2012-02-26 00:01:19 -0300

    Adding portuguese language to the language directory.

---
+++ lang/pt_br.lang.js
@@ -0,0 +1,43 @@
+var JS_BASETEXT = 'Digite seu nome e senha, e &Aacute; clique em entrar.';
+var JS_PROCESSING = 'Processando';
+var JS_LANG = 'pt_br';
+
+var JS_WRONG_EMAIL1 = 'e-mail inv&aacute;lido. Check the @ sign and dots in it.';
+var JS_WRONG_EMAIL2 = 'Wrong e-mail address';
+var JS_WRONG_EMAIL3 = 'O e-mail nao pode conter nenhum IP';
+var JS_WRONG_EMAIL4 = 'Nome do dominio parece ser invalido no e-mail';
+var JS_WRONG_EMAIL5 = 'Falta sufixo no e-mail';
+var JS_WRONG_EMAIL6 = 'Falta hostname no e-mail';
+
+var JS_REG_GIVEYOURNAME = 'Digite um nome de usuario';
+var JS_REG_SPECCHARS = 'O nome nao pode conter acentos nem caracteres especiaas';
+var JS_REG_TOOSHORTNAME = 'Nome curto, deve conter ao menos 5 caracteres';
+var JS_REG_TOOLONGNAME = 'Nome longo, nao deve conter mais de 10 caracteres';
+var JS_REG_GIVEYOURPASSWD = 'Digite uma senha';
+var JS_REG_USERNAMEPASSWDMATCH = 'Seu nome e senha nao podem ser iguais';
+var JS_REG_TOOSHORTPASSWD = 'Nome curto, deve conter 6 caracteres ao menos';
+var JS_REG_REPEATPASSWD = 'Repita a senha';
+var JS_REG_PASSWDNOTMATCH = 'As senhas nao sao iguais';
+var JS_REG_GIVEYOUREMAIL = 'Digite seu e-mail';
+var JS_REG_GIVECAPTCHA = 'Entre os caracteres que voce ve na imagem (captcha).';
+var JS_REG_TOOSHORTCAPTCHA = 'Captcha muito curto';
+var JS_REG_TOOLONGCAPTCHA = 'Captcha muito longo';
+var JS_REG_CHOOSEBORNYEAR = 'Entre seu aniversario';
+var JS_REG_CHOOSEUPSPEED = 'Entre sua velocidade de upload';
+var JS_REG_CHOOSEDOWNSPEED = 'Entre sua velocidade de download';
+var JS_REG_CHOOSEIPS = 'Escolha seu provedor de internet';
+var JS_REG_MUSTREADRULES = 'Voce precisa ler e concordar com as regras desse site.';
+var JS_REG_CHOOSESEX = 'Sexo masculino ou feminino?';
+var JS_REG_CHOOSELANG = 'Escolha seu idioma';
+var JS_REG_CHOOSECOUNTRY = 'Escolha seu pais';
+var JS_PROFILE_CHOOSESTYLE = 'Escolha seu tema';
+var JS_PROFILE_MACHINEON = 'Escolha as horas que voce esta disponivel para download ou upload';
+
+var JS_ERROR = 'Erro!';
+
+var JS_DESCRIPTION 			= 'Descrição';
+var JS_NFO 					= 'NFO';
+var JS_FILES 				= 'Arquivos';
+var JS_COMMENTS 			= 'Comentarios';
+var JS_CONNECTIONSANDSPEED 	= 'Velocidade';
+var JS_HISTORY 				= 'Historico';
